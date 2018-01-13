f06dd8b2bda34061356cb529a0982f217b32466b
Uriel <urychdz@hotmail.es>
2018-01-13 14:00:02 -0600

    1096 - Secuencia IJ 2

---
+++ PRINCIPIANTE/1096.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) 
+{
+    var i = 1
+
+    while (i != 11) 
+    {
+        var j = 7
+
+        while (j > 4) 
+        {
+            println("I=$i J=$j")
+            j -= 1
+        }
+        i += 2
+    }
+}
